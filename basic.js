e6e242ae940e2d2980057b63f78d74aabc6fea05
Acu <wongethan120@gmail.com>
2026-07-12 03:52:19 +0000

    Jul 12, 2026, 3:52 AM

--- basic.js
+++ basic.js
@@ -1,8 +1,10 @@
 function setup(){
     new Canvas(800,400);
     console.log("hello");
-    let base = 10;
-    let height = 5;
+    // let base = 10;
+    // let height = 5;
+    // let area = 0.5 * base * height;
+    // console.log(area);
     
 }
 
