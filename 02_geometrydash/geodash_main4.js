9652ce53f45007c74b5fefdf5690cb0eed110215
Acu <wongethan120@gmail.com>
2026-07-26 03:41:51 +0000

    Jul 26, 2026, 3:41 AM

--- 02_geometrydash/geodash_main4.js
+++ 02_geometrydash/geodash_main4.js
@@ -82,17 +82,6 @@ function setup() {
   new Tiles(tileMap1, 0, 0, 50, 50);
 }
 
-function resetGame() {
-  box.rotation = 0;
-
-  box.x = startCoordinates[0];
-  box.y = startCoordinates[1];
-
-  jumpChance = maxjump;
-
-  camera.x = width;
-}
-
 function draw() {
   clear();
 
@@ -119,6 +108,17 @@ function draw() {
   if (box.collides(sharp)) {
     resetGame();
   }
+}
+
+function resetGame() {
+  box.rotation = 0;
+
+  box.x = startCoordinates[0];
+  box.y = startCoordinates[1];
+
+  jumpChance = maxjump;
+
+  camera.x = width;
 }
 
 
