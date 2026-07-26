0fd2fc581d7ad5038278ea4a24c2bb55de91fe67
Acu <wongethan120@gmail.com>
2026-07-26 03:02:50 +0000

    Jul 26, 2026, 3:02 AM

---
+++ 02_geometrydash/geodash_main4.js
@@ -0,0 +1,94 @@
+//player box
+let box;
+let cube;
+let bg;
+
+// game variables
+
+
+// world building groups
+let tileMap1;
+let ground;
+let orbs;
+let sharp;
+let finishline;
+
+// image sprites
+let spike;
+
+// menu
+
+
+// sound assets
+
+
+function preload() {
+    cube = loadImage('assets/cube.png');
+    bg = loadImage('assets/geobg.png');
+
+    tileMap1 = loadStrings('stages/tiles1.txt');
+    spike = loadImage('assets/spike.png');
+}
+
+function setup() {
+  new Canvas(700,600);
+  world.gravity.y = 32
+
+  box = new Sprite(50, height, 50, 50);
+  box.img = cube;
+  box.friction = 0;
+  box.bounciness = 0;
+  box.collider = "None";
+
+  startCoordinates = [50, height - box.height/2];
+  box.x = startCoordinates[0];
+  box.y = startCoordinates[1];
+
+  ground = new Group();
+  ground.tile = "g";
+
+  ground.w = 50;
+  ground.h = 50;
+  ground.collider = "static";
+  ground.color = "black";
+  ground.stroke = "rgba(0,0,0,0)";
+
+  orbs = new Group();
+  orbs.tile = "o";
+
+  orbs.d = 24;
+  orbs.collider = "static";
+  orbs.color = "white";
+  orbs.strokeWeight = 0;
+
+  finishline = new Group();
+  finishline.tile = "f";
+  finishline.w = 50;
+  finishline.h = 1200;
+  finishline.visible = false;
+  finishline.collider = "static";
+
+  spike = new Group();
+  spike.tile = "s";
+  spike.w = 50;
+  spike.h = 50;
+  spike.collider = "static";
+  spike.color = "black";
+
+  new Tiles(tileMap1, 0, 0, 50, 50);
+}
+
+function draw() {
+  
+}
+
+
+
+
+
+
+
+
+
+
+
