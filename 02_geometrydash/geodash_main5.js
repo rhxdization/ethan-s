bffdf2df5ecd3fe7bf7df3c3e80f03ae0af6a93e
Acu <wongethan120@gmail.com>
2026-08-02 04:21:12 +0000

    Aug 2, 2026, 4:21 AM

--- 02_geometrydash/geodash_main5.js
+++ 02_geometrydash/geodash_main5.js
@@ -100,7 +100,7 @@ function draw() {
   clear();
   image(bg,0,0,width,height);
 
-  if (!startgame && (mousepresses))
+  if (!startgame && (mouse.presses()) || kb.presses)
 
   box.collider = "dynamic";
   box.vel.x = 8;
