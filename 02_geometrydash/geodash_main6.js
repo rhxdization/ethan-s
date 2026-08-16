07d77e6920e3feb61eb517ec8e9f249fa9d63d05
Acu <wongethan120@gmail.com>
2026-08-16 03:46:54 +0000

    Aug 16, 2026, 3:46 AM

--- 02_geometrydash/geodash_main6.js
+++ 02_geometrydash/geodash_main6.js
@@ -51,7 +51,7 @@ function preload() {
     tilemap2 = loadStrings("stages/tiles2.txt");
 
     backgroundtrack = createAudio("assets/stereo-madness.mp3");
-    failsound = createAudio()
+    failsound = createAudio("assets/geometry-dash-death-sound.mp3")
 }
 
 function setup() {
