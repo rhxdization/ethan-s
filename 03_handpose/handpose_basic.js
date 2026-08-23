a82e7f98822eba66fcf548cc7e5321c23f44c3ff
Acu <wongethan120@gmail.com>
2026-08-23 04:11:57 +0000

    Aug 23, 2026, 4:11 AM

--- 03_handpose/handpose_basic.js
+++ 03_handpose/handpose_basic.js
@@ -57,17 +57,26 @@ function gotHands(results) {
 function draw() {
     image(video, 0, 0, videoW, videoH);
 
-    for (let i = 0; i < hands.length; i++) {
-        let hand = hands[i];
+//     for (let i = 0; i < hands.length; i++) {
+//         let hand = hands[i];
 
-        for (let j = 0; j < hand.keypoints.length; j++) {
-            let keypoint = hand.keypoints[j];
-            circle(keypoint.x, keypoint.y, 10);
-        }
+//         for (let j = 0; j < hand.keypoints.length; j++) {
+//             let keypoint = hand.keypoints[j];
+//             circle(keypoint.x, keypoint.y, 10);
+//         }
         
-    }
+//     }
 
+// }
+
+if (hands.length > 0) {
+    let hand = hands[0];
+    let keypoint = hand.keypoints[8];
+
+    circle(keypoint.x, keypoint.y, 30);
 }
+}
+
 
 //=========================================
 // Function Created
