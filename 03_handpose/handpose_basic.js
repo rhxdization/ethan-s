beb7063a8a930bbe453780df1b8556c7552e0a27
Acu <wongethan120@gmail.com>
2026-08-23 03:56:01 +0000

    Aug 23, 2026, 3:56 AM

--- 03_handpose/handpose_basic.js
+++ 03_handpose/handpose_basic.js
@@ -62,9 +62,9 @@ function draw() {
 
         for (let j = 0; j < hand.keypoints.length; j++) {
             let keypoint = hand.keypoints[j];
+            circle(keypoint.x, keypoint.y, 10);
         }
         
-        circle(keypoint.x, keypoint.y, 10);
     }
 
 }
