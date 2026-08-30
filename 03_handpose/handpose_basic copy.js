d254dc7496c89cb72bcf716d74be766419c5f393
Acu <wongethan120@gmail.com>
2026-08-30 04:14:49 +0000

    Aug 30, 2026, 4:14 AM

---
+++ 03_handpose/handpose_basic copy.js
@@ -0,0 +1,103 @@
+//=========================================
+// Variables
+//=========================================
+
+let handpose;
+let videoH = 480;
+let videoW = 640;
+let hands = [];
+let balloon;
+
+
+
+//=========================================
+// Code
+//=========================================
+
+function preload() {
+    let options = {
+        flipped: true,
+        runtime: "tfjs",
+        modelType: "full",
+        detectorModelUrl: undefined,
+        landmarkModelUrl: undefined,
+    }
+
+    handpose = ml5.handPose(options);
+    
+}
+
+
+function setup() {
+    createCanvas(videoW, videoH);
+
+    let constraints = {
+        video: {
+            mandatory: {
+                minWidth: videoW,
+                minHeight: videoH,
+            },
+            optional: [{minFrameRate: 60}]
+        },
+        audio: false,
+        flipped: true,
+    };
+
+    video = createCapture(constraints);
+    video.size(640,480);
+    video.hide();
+
+    handpose.detectStart(video, gotHands);
+
+    fingertip = new Sprite();
+    fingertip.diameter = 60;
+    fingertip.collider = 'kinematic';
+    fingertip.color = 'rgba(0,255,0,0.05';
+
+    balloon = new Sprite();
+    balloon.diameter = 50;
+    balloon.collider = 'dynamic';
+    balloon.color = 'red';
+    balloon.x = width/2;
+    balloon.y = 100;
+    balloon.bounciness = 1;
+    balloon.mass - 2;
+    balloon.drag = 0.01;
+}
+
+function gotHands(results) {
+    hands = results
+}
+
+function draw() {
+    image(video, 0, 0, videoW, videoH);
+
+//     for (let i = 0; i < hands.length; i++) {
+//         let hand = hands[i];
+
+//         for (let j = 0; j < hand.keypoints.length; j++) {
+//             let keypoint = hand.keypoints[j];
+//             circle(keypoint.x, keypoint.y, 10);
+//         }
+        
+//     }
+
+    if (hands.length > 0) {
+        let hand = hands[0];
+        let keypoint = hand.keypoints[8];
+
+        circle(keypoint.x, keypoint.y, 30);
+
+        fingertip.x = keypoint.x;
+        fingertip.y = keypoint.y;
+        fingertip.visible = true;
+    }
+    else {
+        fingertip.visible = false;
+    }
+}
+
+
+//=========================================
+// Function Created
+//=========================================
