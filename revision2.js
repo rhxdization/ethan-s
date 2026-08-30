4688b0164528b0bde8aeaa6fcb1ccefb3b74ec23
Acu <wongethan120@gmail.com>
2026-08-30 03:53:15 +0000

    Aug 30, 2026, 3:53 AM

---
+++ revision2.js
@@ -0,0 +1,28 @@
+let box;
+let platform;
+
+function setup() {
+    new Canvas(640,480);
+
+    
+
+    box = new Sprite(100, height/2, 50, 50);
+    // box.collision = 'dynamic';
+    // box.friction = 0;
+    // box.bounciness = 0;
+    box.color = 'rgb(255,0,0)';
+
+    platform = new Sprite(320, 480, 1000, 50);
+    platform.collision = 'static';
+    platform.color = 'rgb(0,0,0)';
+
+    world.gravity.y = 25;
+
+}
+
+
+function draw(){
+    background("blue");
+
+    box.velocity.x = 10
+}
