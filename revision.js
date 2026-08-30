63c7599952d4b3b95b63b039a56caf649ec7135b
Acu <wongethan120@gmail.com>
2026-08-30 03:29:45 +0000

    Aug 30, 2026, 3:29 AM

--- revision.js
+++ revision.js
@@ -4,6 +4,8 @@ let platform;
 function setup() {
     new Canvas(640,480);
 
+    world.gravity.y = 25;
+
     box = new Sprite(100, height/2, 50, 50);
     box.collision = 'dynamic';
     box.color = 'rgb(255,0,0)';
