bbfc0ad4de94f083ca2cb31651d90e2e404c9414
Acu <wongethan120@gmail.com>
2026-09-13 03:16:19 +0000

    Sep 13, 2026, 3:16 AM

--- 03_handpose/handpose_basic_4.js
+++ 03_handpose/handpose_basic_4.js
@@ -132,7 +132,7 @@ function draw() {
         fill('limegreen');
         textSize(20);
         text("Use Index Finger to bounce the ball.",width/2,height/2-40);
-        textsize(28);
+        textSize(28);
         text("Press SPACE to start the game",width/2,height/2);
     }
 
