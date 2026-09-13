6484faa5f08382ecd0c95cf6b42a1400d4819628
Acu <wongethan120@gmail.com>
2026-09-13 03:18:08 +0000

    Sep 13, 2026, 3:18 AM

--- 03_handpose/handpose_basic_4.js
+++ 03_handpose/handpose_basic_4.js
@@ -150,5 +150,10 @@ function keypressed() {
         score = 0;
     }
 
-    balloon.x = 
+    balloon.x = width/2;
+    balloon.y = 100;
+    balloon.vel.x = 0;
+    balloon.vel.y = 0;
+    balloon.collider = "dynamic";
+    
 }
