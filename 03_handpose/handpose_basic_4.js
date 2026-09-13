3d5440f6e94aab7a9db69eadc2d516c2f7104cea
Acu <wongethan120@gmail.com>
2026-09-13 04:06:08 +0000

    Sep 13, 2026, 4:06 AM

--- 03_handpose/handpose_basic_4.js
+++ 03_handpose/handpose_basic_4.js
@@ -29,7 +29,7 @@ function preload() {
     let options = {
         flipped: true,
         runtime: "tfjs",
-        modelType: "full",
+        modelType: "lite",
         detectorModelUrl: undefined,
         landmarkModelUrl: undefined,
     }
