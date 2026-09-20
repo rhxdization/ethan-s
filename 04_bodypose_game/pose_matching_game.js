2db4ecb544d800145ee05f477645c66558203e6d
Acu <wongethan120@gmail.com>
2026-09-20 03:53:28 +0000

    Sep 20, 2026, 3:53 AM

--- 04_bodypose_game/pose_matching_game.js
+++ 04_bodypose_game/pose_matching_game.js
@@ -68,7 +68,7 @@ function draw() {
     // Draw the middle line that separates Player 1 and Player 2 areas.
     drawMiddleLine();
 
-    Image(video, cameraX, 0,)
+    Image(video, cameraX, 0, cameraWidth, cameraHeight);
 
 }
 
