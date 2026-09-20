10943ae22cba51106779564e5f7a79063baf4fca
Acu <wongethan120@gmail.com>
2026-09-20 03:40:19 +0000

    Sep 20, 2026, 3:40 AM

--- 04_bodypose_game/pose_matching_game.js
+++ 04_bodypose_game/pose_matching_game.js
@@ -37,7 +37,7 @@ function preload(){
 
 // setup() runs once at the start.
 function setup() {
-    new Canvas
+    new Canvas(totalCanvasWidth, cameraHeight);
     // Set up text.
     textAlign(CENTER, CENTER);
 }
