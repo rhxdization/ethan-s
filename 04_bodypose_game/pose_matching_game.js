14e6189e9bf60e802c8caaac648c8959f4221429
Acu <wongethan120@gmail.com>
2026-09-20 03:54:23 +0000

    Sep 20, 2026, 3:54 AM

--- 04_bodypose_game/pose_matching_game.js
+++ 04_bodypose_game/pose_matching_game.js
@@ -24,6 +24,7 @@ let leftPanelX = 0;
 let rightPanelX = sidePanelWidth + cameraWidth;
 
 let bodypose;
+let detectedPeople = [];
 
 // ====================================================
 // Preload
